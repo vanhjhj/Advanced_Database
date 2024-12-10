ff3a5d56585b1c7fe784cdd438b8dc81c21a1708
Nguyen Dang Tri <22120383@student.hcmus.edu.vn>
2024-12-10 12:03:08 +0700

    add Card relative features

---
+++ Source code/SuShiX/SuShiX/FrmCreateAndReissueCard.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SuShiX
+{
+    public partial class FrmCreateAndReissueCard : Form
+    {
+
+        public string userName { get; set; }
+
+        public FrmCreateAndReissueCard()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txbUserName.Text))
+            {
+                MessageBox.Show("Vui lòng nhập tên tài khoản", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            userName = txbUserName.Text;
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+        }
+
+        private void pbReturn_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Cancel;
+            this.Close();
+        }
+    }
+}
--- Source code/SuShiX/SuShiX/Program.cs
+++ Source code/SuShiX/SuShiX/Program.cs
@@ -17,8 +17,9 @@ namespace SuShiX
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             //Application.Run(new FrmLogin());
-            //Application.Run(new FrmEmployee("NV0020"));
+            Application.Run(new FrmEmployee("NV0020"));
             //Application.Run(new FrmManager("NV0000"));
+            //Application.Run(new FrmCreateAndReissueCard());
         }
     }
 }
