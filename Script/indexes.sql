ca12bd551f5fea995637f9a49cfa6f0b32e8c1df
vunewbie <vulocninh1@gamil.com>
2024-12-19 01:03:36 +0700

    pull from remote

--- Script/indexes.sql
+++ Script/indexes.sql
@@ -1,12 +1,12 @@
-﻿--Tạo Non-clustered Index cho PhieuDat (TinhTrangThanhToan, LoaiPD, MaCN)
-CREATE NONCLUSTERED INDEX Idx_PhieuDat_TinhTrangThanhToan_LoaiPD_MaCN ON dbo.PhieuDat (TinhTrangThanhToan, LoaiPD, MaCN);
+﻿--Tạo Non-clustered Index cho PhieuDat (MaCN, LoaiPD, TinhTrangThanhToan)
+CREATE NONCLUSTERED INDEX Idx_PhieuDat_MaCN_LoaiPD_TinhTrangThanhToan ON dbo.PhieuDat (MaCN, LoaiPD, TinhTrangThanhToan);
 	
 	--Thực thi procedures để kiểm tra Execution Plan
 EXEC dbo.USP_DanhSachDat @LoaiPhieuDat = N'Đặt Bàn Trực Tuyến', -- nvarchar(20)
                          @MaTKNhanVien = 'NV000040'   -- varchar(10)
 	
 	--Drop Index
---DROP INDEX Idx_PhieuDat_TinhTrangThanhToan_LoaiPD_MaCN ON dbo.PhieuDat;
+--DROP INDEX Idx_PhieuDat_MaCN_LoaiPD_TinhTrangThanhToan ON dbo.PhieuDat;
 
 
 --=======================================================================================================================
@@ -24,16 +24,15 @@ EXEC dbo.USP_XemThongTinKhachHang @MaTK = 'KH044999' -- varchar(10)
 
 --Tạo Non-clustered Index cho CTPD (MaMA) INCLUDE (SoLuong, ThanhTien)
 CREATE NONCLUSTERED INDEX Idx_CTPD_MaMA ON dbo.CTPD (MaMA) INCLUDE (SoLuong, ThanhTien);
-
+--CREATE NONCLUSTERED INDEX Idx_PhieuDat_MaCN_LoaiPD_TinhTrangThanhToan ON dbo.PhieuDat (MaCN, LoaiPD, TinhTrangThanhToan);
 	--Thực thi procedures để kiểm tra Execution Plan
 DECLARE @TongSoLuongBan INT,
         @TongDoanhThu BIGINT;
-EXEC dbo.USP_QuanLyThongKe @MaTK = 'NV0012',                         -- varchar(10)
-                           @NgayBD = '2019-12-14 20:15:10',          -- datetime
-                           @NgayKT = '2024-12-14 20:15:10',          -- datetime
-                           @TenMA = N'SAKE POTETO CHIIZU'			 -- nvarchar(100)
+EXEC dbo.USP_QuanLyThongKe @MaTK = 'NV000012',                         -- varchar(10)
+                           @NgayBD = '2010-01-01 00:00:00',          -- datetime
+                           @NgayKT = '2016-12-31 00:00:00',          -- datetime
+                           @TenMA = N'HOKKIGAI SASHIMI',			 -- nvarchar(100)
                            @TongSoLuongBan = @TongSoLuongBan OUTPUT, -- int
                            @TongDoanhThu = @TongDoanhThu OUTPUT      -- bigint
-
 	--Drop Index
 --DROP INDEX Idx_CTPD_MaMA ON dbo.CTPD;
