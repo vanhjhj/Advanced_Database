f80cfed204b41675078e130a03cbd25896f0c0b3
vunewbie <vulocninh1@gamil.com>
2024-12-18 23:30:18 +0700

    pull from remote

--- Script/indexes.sql
+++ Script/indexes.sql
@@ -3,7 +3,7 @@ CREATE NONCLUSTERED INDEX Idx_PhieuDat_TinhTrangThanhToan_LoaiPD_MaCN ON dbo.Phi
 	
 	--Thực thi procedures để kiểm tra Execution Plan
 EXEC dbo.USP_DanhSachDat @LoaiPhieuDat = N'Đặt Bàn Trực Tuyến', -- nvarchar(20)
-                         @MaTKNhanVien = 'NV0040'   -- varchar(10)
+                         @MaTKNhanVien = 'NV000040'   -- varchar(10)
 	
 	--Drop Index
 --DROP INDEX Idx_PhieuDat_TinhTrangThanhToan_LoaiPD_MaCN ON dbo.PhieuDat;
@@ -15,7 +15,7 @@ EXEC dbo.USP_DanhSachDat @LoaiPhieuDat = N'Đặt Bàn Trực Tuyến', -- nvarc
 CREATE NONCLUSTERED INDEX Idx_The_TkSoHuu ON dbo.The (TkSoHuu);
 
 	--Thực thi procedures để kiểm tra Execution Plan
-EXEC dbo.USP_XemThongTinKhachHang @MaTK = 'KH44999' -- varchar(10)
+EXEC dbo.USP_XemThongTinKhachHang @MaTK = 'KH044999' -- varchar(10)
 
 	--Drop Index
 --DROP INDEX Idx_The_TkSoHuu ON dbo.The;
